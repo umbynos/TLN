31c17582076bb86b3cb508a934dafff9c304bf10
umbynos <umberto.baldi@edu.unito.it>
2019-04-25 16:32:12 +0200

    remove debug prints and fixed bugs, now it works

--- cky/cky_algorithm.py
+++ cky/cky_algorithm.py
@@ -9,25 +9,19 @@ def cky_parse(words,grammar_dict):
 			table[a,b] = []
 	#cky-algorithm beginning
 	for j in range(1, len(words)+1):
-		print("j: " + str(j))
-		table[j-1,j-1] = find_lex_rule(words[j-1], grammar_dict)
-		print(table)
+		table[j-1,j-1].append(find_lex_rule(words[j-1], grammar_dict))
 		for i in range(j-2, -1, -1): #third argument "-1" used as decrement var
-			print ("i: " + str(i))
 			for k in range(i+1, j): #j: because range does not include last elem (j-1)
-				print ("k: " + str(k))
-				for z in range (len(table[i,k])):
-					b = table[i,k][z]
+				for z in range (len(table[i,k-1])):
+					b = table[i,k-1][z]
 					for x in range (len(table[k,j-1])):
-						c = table[k,j-1]
+						c = table[k,j-1][x]
 						rhs = b + " " + c
 						if (rhs in grammar_dict):
 							table[i,j-1].append(grammar_dict[rhs])
 	return table
 
 def find_lex_rule(word,grammar_dict):
-	print(word)
-	print(grammar_dict[word])
 	return grammar_dict[word]
 
 def create_dict(grammar_file):
