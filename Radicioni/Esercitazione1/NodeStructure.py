0ae8e8ed067855ac0310cd181fd729860528686b
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-06-04 09:41:39 +0200

    add initial part of es 1

---
+++ Radicioni/Esercitazione1/NodeStructure.py
@@ -0,0 +1,9 @@
+# Node structure containing words and similarity misure
+
+class Node:
+	def __init__(self, fst_word, snd_word):
+		self.value = value
+		self.left_child = None
+		self.right_child = None
+		#leaf used to keep the value of the words
+		self.leaf_child = None 
