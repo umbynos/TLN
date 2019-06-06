36cc37720842f807cf213110066253eb8ea7d271
umbynos <umberto.baldi@edu.unito.it>
2019-06-07 01:17:20 +0200

    finish implementation of lesk algo, still lot to do

--- progetto_radicioni/esercitazione_2/wsd.py
+++ progetto_radicioni/esercitazione_2/wsd.py
@@ -1,7 +1,8 @@
-import lesk_algoritm as lesk
+import lesk_algorithm as lesk
 
 def main():
 	#serve un parsificatore che riconosca ** e che vada poi a chiamare lesk_algo
+	lesk.simplifiedLesk("Arms", "Arms bend at the elbow.")
 
 if __name__== "__main__":
-main()
+	main()
