c16e8472c1aa889880420cf05041fc457bc46be0
umbynos <umberto.baldi@edu.unito.it>
2019-06-07 01:17:20 +0200

    finish implementation of lesk algo, still lot to do

--- progetto_radicioni/esercitazione_2/lesk_algorithm.py
+++ progetto_radicioni/esercitazione_2/lesk_algorithm.py
@@ -1,24 +1,35 @@
 from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+import string
 
-def SimplifiedLesk(word,sentence): #word->termine polisemico; sentence->contesto
+stop = stopwords.words('english') + list(string.punctuation)
+
+def simplifiedLesk(word,sentence): #word->termine polisemico; sentence->contesto
 	best_sense = most_frequent_sense(word) # il primo elencato in wordnet
 	max_overlap = 0
 	context = word_set(sentence)
+	print(context)
 	for sense in senses(word):
-		signature = #? set of words in the gloss and examples of sense
+		signature = {i for i in word_tokenize(sense.definition() + ' '.join(sense.examples())) if i not in stop}#? set of words in the gloss and examples of sense
+		print(signature)
 		overlap = compute_overlap(signature, context) #returns the number of words in common between two sets, ignoring function words or other words on a stop list
-		if (overlap > max_overlap)
+		print(overlap)
+		if (overlap > max_overlap):
 			max_overlap = overlap
 			best_sense = sense
+	print(best_sense)
 	return best_sense
 
 def most_frequent_sense(word):
-	wn.synsets(word)[0]
+	return wn.synsets(word)[0]
 
 def word_set(sentence):
-	set(sentence.split(" "))
+	return {i for i in word_tokenize(sentence) if i not in stop}
 
 def senses(word):
-	wn.synsets(word)
+	print(wn.synsets(word))
+	return wn.synsets(word)
 
 def compute_overlap(signature, context):
+	return len(signature.intersection(context))
