060e4103761157476c288f8e745277780a2d1462
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-06-06 18:41:21 +0200

    modify wu_and_palmer method to analyze words instead of senses. NEXT TIME: modify the other two so taht they use words and they control the loop

--- progetto_radicioni/esercitazione_1/node_structure.py
+++ progetto_radicioni/esercitazione_1/node_structure.py
@@ -1,10 +1,13 @@
 # Node structure containing words and similarity misure
+# NOTA: node_structure come nome non va bene: sono inseriti in un array, non sono collegati fra di loro!!!!!!!!!!!!!
 
 class Node:
 	def __init__(self, fst_word, snd_word, similarity):
 		self.fst_word = fst_word
 		self.snd_word = snd_word
 		self.similarity = similarity
+		self.fst_word_sense = None
+		self.snd_word_sense = None
 
 	def get_fst_word(self):
 		return self.fst_word
@@ -15,14 +18,26 @@ class Node:
 	def get_similarity(self):
 		return self.similarity
 
-	def set_fst_word(fst_word):
+	def get_fst_word_sense(self):
+		return self.fst_word_sense
+
+	def get_snd_word_sense(self):
+		return self.snd_word_sense
+
+	def set_fst_word(self, fst_word):
 		self.fst_word = fst_word
 
-	def set_snd_word(snd_word):
+	def set_snd_word(self, snd_word):
 		self.snd_word = snd_word
 
-	def set_similarity(similarity):
+	def set_similarity(self, similarity):
 		self.similarity = similarity
+
+	def set_fst_word_sense(self, sense):
+		self.fst_word_sense = sense
+
+	def set_snd_word_sense(self, sense):
+		self.snd_word_sense = sense
 
 	def print_node(self):
 		print(self.fst_word, self.snd_word, self.similarity) 
