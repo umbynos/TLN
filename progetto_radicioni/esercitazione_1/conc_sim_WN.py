e9fdb74dd30c55534dd43c3ff59a72adda597f70
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-06-07 18:01:05 +0200

    wup ok, shortest_path ok, leakcock_chodorow NOT ok

--- progetto_radicioni/esercitazione_1/conc_sim_WN.py
+++ progetto_radicioni/esercitazione_1/conc_sim_WN.py
@@ -45,6 +45,8 @@ def main():
 		node = Node(fst_word, snd_word, similarity)
 		words_array.append(node)
 	file.close()
+	global depth_max
+	depth_max = depthMax()
 	#s1, s2 = words_to_best_senses(words_array[5].get_fst_word(),words_array[5].get_snd_word())
 	#print("Wu & Palmer:", wu_and_palmer(s1, s2))
 	#print("Shortest path:", shortest_path(s1, s2)) # CONFRONTARE CON QUELLO DELLA LIBRERIA
@@ -60,49 +62,99 @@ def main():
 #		print("i,s1,s2", i,s1,s2)
 #		elem.set_fst_word_sense(s1)
 #		elem.set_snd_word_sense(s2)
-	# Creating array with Wu & Palmer similarity for each entry of words_array
-	# CICLO QUA E NON DENTRO A WU_AND_PALMER PERCHE LA DEFINIZIONE UFFICIALE PRENDE COME ARGOMENTO SOLO DUE SENSI (E COSI ANCHE GLI ALTRI)
+	# Creating array with Wu & Palmer Similarity for each entry of words_array
 	wu_and_palmer_array = []
 	for i in range(len(words_array)):
 		word1 = words_array[i].get_fst_word()
 		word2 = words_array[i].get_snd_word()
 		wu_and_palmer_array.append(wu_and_palmer(word1,word2))
 		print("i,wu_and_palmer_array:", i,wu_and_palmer_array[i])
+	# Creating array with Short Path Similarity for each entry of words_array
+	shortest_path_array = []
+	for i in range(len(words_array)):
+		word1 = words_array[i].get_fst_word()
+		word2 = words_array[i].get_snd_word()
+		shortest_path_array.append(shortest_path(word1,word2))
+		print("i,shortest_path_array:", i,shortest_path_array[i])
+	# Creating array with Short Path Similarity for each entry of words_array
+	leakcock_chodorow_array = []
+	for i in range(len(words_array)):
+		word1 = words_array[i].get_fst_word()
+		word2 = words_array[i].get_snd_word()
+		leakcock_chodorow_array.append(leakcock_chodorow(word1,word2))
+		print("i,leakcock_chodorow_array:", i,leakcock_chodorow_array[i])
 
 #def spearman(): # NOTA DA FARE SU TUTTO IL FILE, NON SULLE SINGOLE RIGHE
 
-# wu_and_palmer takes two words and returns te Wu & Palmer Similarity considering the two sense that give the maximum similarity
+# QUESTI 3 METODI CI PIACCIONO COSI O PREFERIAMO CREARNE UNO SOLO A CUI SI PASSANO LE DUE PAROLE ED UN PARAMETRO CHE INDENTIFICA UNO DEI TRE METODI?
+# wu_and_palmer takes two words and returns the Wu & Palmer Similarity considering the two senses that give the maximum similarity
 def wu_and_palmer(word1, word2):
 	synset1 = wn.synsets(word1)
 	synset2 = wn.synsets(word2)
-	sense1 = ""
-	sense2 = ""
 	cs = 0
-	for s1 in synset1:
-		for s2 in synset2:	
-			# LCS: Lowest Common Subsumer. It is the lower common ancestor between sense1 and sense2, id est the lowest common hypernym
-			lcs = s1.lowest_common_hypernyms(s2, simulate_root=True) # VA BENE O DOBBIAMO CALCOLARCELO NOI??? PRIMA DI FARLO MANDEREI UNA MAIL
-			# simulate_root=True: fake root that connect the taxonomies that do not share a single root
-			if(s1.min_depth()!=0 or s2.min_depth()!=0): #CONTROLLO OBBLIGATORIO: CAPITA. NON SO PERCHE
-				new_cs = 2*lcs[0].min_depth()/(s1.min_depth()+s2.min_depth())
-				# min_depth() returns the length of the shortest hypernym path from this synset to the root
+	if(len(synset1)>0 and len(synset2)>0): # some words in the file WordSim353.tab are not present in WordNet DataBase
+		for s1 in synset1:
+			for s2 in synset2:	
+				# LCS: Lowest Common Subsumer. It is the lower common ancestor between sense1 and sense2, id est the lowest common hypernym
+				lcs = s1.lowest_common_hypernyms(s2, simulate_root=True, use_min_depth=True) # VA BENE O DOBBIAMO CALCOLARCELO NOI??? PRIMA DI FARLO MANDEREI UNA MAIL
+				# simulate_root=True: fake root that connect the taxonomies that do not share a single root
+				# use_min_depth=True: USATO PERCHE USATO NELLA LIBRERIA DI WN. SENZA NON TUTTI SONO UGUALI. LO TENIAMO? SPIEGAZIONE:
+				# Note that to preserve behavior from NLTK2 we set use_min_depth=True
+		        # It is possible that more accurate results could be obtained by
+		        # removing this setting and it should be tested later on
+				new_cs = 2*(lcs[0].max_depth()+1) / ((s1.max_depth()+1) + (s2.max_depth()+1))
+				# Get the longest path from the LCS to the root, with correction: add one because the calculations include both the start and end nodes
 				if(new_cs > cs):
 					cs = new_cs
-	#print("Valore di WN per wup:", sense1.wup_similarity(sense2))
+					sense1 = s1
+					sense2 = s2
+		print("wu_and_palmer WD: ", sense1.wup_similarity(sense2, simulate_root=True))
 	return cs
 	# VALORI RESTITUITI NON UGUALI: ALCUNI MOLTO DIVERSI, ALTRI POCO
 
-# shortest_path() returns the shortest path length from sense1 to sense2 (the parmeters)
-def shortest_path(sense1,sense2):
-	return 2*depthMax()-sense1.shortest_path_distance(sense2, simulate_root=True)
+# shortest_path() returns the shortest path length from word1 to word2 considering the two senses that give the maximum similarity
+def shortest_path(word1,word2):
+	synset1 = wn.synsets(word1)
+	synset2 = wn.synsets(word2)
+	sim = 0
+	if(len(synset1)>0 and len(synset2)>0): # some words in the file WordSim353.tab are not present in WordNet DataBase
+		for s1 in synset1:
+			for s2 in synset2:
+				new_sim = 2*depth_max-s1.shortest_path_distance(s2, simulate_root=True)
+				if(new_sim > sim):
+					sim = new_sim
+					#sense1 = s1
+					#sense2 = s2
+		#print("shortest_path WD: ", sense1.path_similarity(sense2))
+		# NON HO TROVATO IL CORRISPETTIVO IN WN
+	return sim
 
-# leakock_chodorow() returns the Leakcock Chodorow Similarity
-def leakcock_chodorow(sense1, sense2):
-	len = sense1.shortest_path_distance(sense2, simulate_root=True)
-	if(len == 0):
-		return -math.log((len+1)/(2*depthMax()+1))
+# leakock_chodorow() returns the Leakcock Chodorow Similarity considering the two senses that give the maximum similarity
+def leakcock_chodorow(word1, word2):
+	synset1 = wn.synsets(word1)
+	synset2 = wn.synsets(word2)
+	sim = 0
+	found = False # the similarity is computed only if the parameters have at least one sense in common with the same POS
+	if(len(synset1)>0 and len(synset2)>0): # some words in the file WordSim353.tab are not present in WordNet DataBase
+		for s1 in synset1:
+			for s2 in synset2:
+				length = s1.shortest_path_distance(s2, simulate_root=True)
+				if(length == 0):
+					new_sim = abs(math.log((length+1)/(2*depth_max+1)))
+				else:
+					new_sim =  abs(math.log(length/2*depth_max))
+				if(new_sim>sim and s1._pos==s2._pos):
+					sim = new_sim
+					sense1 = s1
+					sense2 = s2
+					found = True
+	if(found):
+		print("leakock_chodorow WD: ", sense1.lch_similarity(sense2))
+		return sim
 	else:
-		return -math.log(len/2*depthMax())
+		print(found)
+		return -1
+	
 
 # It takes two words and it returns the senses with the highest Wu & Palmer similarity
 #def words_to_best_senses(fst_word, snd_word):
