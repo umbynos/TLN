12439ac15544e17ae37916335c40515edeac61d8
umbynos <umberto.baldi@edu.unito.it>
2019-06-08 17:42:05 +0200

    almost finished second point of the es

--- progetto_radicioni/esercitazione_2/wsd.py
+++ progetto_radicioni/esercitazione_2/wsd.py
@@ -1,15 +1,26 @@
 import lesk_algorithm as lesk
+import re
 
 path = "utils/sentences.txt" # path of the file which contains the sentences
 
 def main():
+	sentences_list=[]
+	word_list=[]
 	sentences_file = open(path,'r')
-	sentences_list = sentences_file.readlines()
-	sentences_list = [x for x in sentences_list if x != '\n'] #remove '\n' from the list
-	#sentences_list = [re.compile(r"\n").sub("", m) for m in sentences_list] #finire regex per filtrare la lista e poi **
-	print(sentences_list)
-	#lesk.simplifiedLesk("Arms", "Arms bend at the elbow.")
+	for i,line in enumerate(sentences_file):
+		if line != '\n' and not line.lstrip().startswith('#'): # ignora i commenti e gli a capo
+			word_list.append(re.search("(\*\*)(.*)(\*\*)", line).group(2)) # cerca la stringa compresa tra gli **
+			sentences_list.append(line.replace('- ', '').replace('*', '').replace('.', '').replace('\n','')) # toglie i caratteri inutili dalla stringa
 	sentences_file.close()
+
+	for i,sentence in enumerate(sentences_list):
+		print("---------------------------------------------------------------------------------")
+
+		print(sentences_list[i])
+		print(word_list[i])
+		print()
+		lesk.simplifiedLesk(word_list[i], sentences_list[i])
+		print("---------------------------------------------------------------------------------")
 
 if __name__== "__main__":
 	main()
