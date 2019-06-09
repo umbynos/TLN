7514f37fc44a660b4df94e9e3d6de048eb4e5c92
umbynos <umberto.baldi@edu.unito.it>
2019-06-09 14:15:40 +0200

    finish second point's implementation (synonims)

--- progetto_radicioni/esercitazione_2/wsd.py
+++ progetto_radicioni/esercitazione_2/wsd.py
@@ -4,23 +4,22 @@ import re
 path = "utils/sentences.txt" # path of the file which contains the sentences
 
 def main():
-	sentences_list=[]
-	word_list=[]
 	sentences_file = open(path,'r')
 	for i,line in enumerate(sentences_file):
 		if line != '\n' and not line.lstrip().startswith('#'): # ignora i commenti e gli a capo
-			word_list.append(re.search("(\*\*)(.*)(\*\*)", line).group(2)) # cerca la stringa compresa tra gli **
-			sentences_list.append(line.replace('- ', '').replace('*', '').replace('.', '').replace('\n','')) # toglie i caratteri inutili dalla stringa
+			sentence = line.replace('- ', '').replace('*', '').replace('.', '').replace('\n','') # toglie i caratteri inutili dalla stringa
+			word = re.search("(\*\*)(.*)(\*\*)", line).group(2) # cerca la stringa compresa tra gli **
+			print("---------------------------------------------------------------------------------")
+			print(sentence)
+			print(word)
+			print()
+			best_sense = lesk.simplifiedLesk(word, sentence)
+			print("---------------------------------------------------------------------------------")
+			for lemma in best_sense.lemma_names():
+				print(re.sub("(\*\*)(.*)(\*\*)", lemma, line))
+
+
 	sentences_file.close()
-
-	for i,sentence in enumerate(sentences_list):
-		print("---------------------------------------------------------------------------------")
-
-		print(sentences_list[i])
-		print(word_list[i])
-		print()
-		lesk.simplifiedLesk(word_list[i], sentences_list[i])
-		print("---------------------------------------------------------------------------------")
 
 if __name__== "__main__":
 	main()
