2b26f340087abdaa9d7c34188fd231ee21470e1a
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-07-01 11:24:41 +0200

    update shortest path similarity: with equal words it returns 2*depthMax. According to Phil, still problem with correlation indexes of shortest path

--- progetto_radicioni/esercitazione_1/conc_sim_WN.py
+++ progetto_radicioni/esercitazione_1/conc_sim_WN.py
@@ -20,6 +20,8 @@ def main():
 	# Depth of WordNet structure
 	global depth_max
 	depth_max = depthMax()
+	print("profondita max", depth_max)
+	print("shortest_path_sim",shortest_path_sim("tiger","tiger"))
 	# Computing similarities
 	wu_and_palmer_sim_indexes = [] # List with Wu & Palmer Similarity for each entry of words_array
 	shortest_path_sim_indexes = [] # List with Shortest Path Similarity for each entry of words_array
@@ -32,8 +34,8 @@ def main():
 		wu_and_palmer_sim_indexes.append(float(wu_and_palmer(word1,word2)))
 		print("wu_and_palmer nostro:", wu_and_palmer_sim_indexes[i])
 		# Shortest Path Similarity
-		shortest_path_sim_indexes.append(shortest_path(word1,word2))
-		print("shortest_path:", shortest_path_sim_indexes[i])
+		shortest_path_sim_indexes.append(shortest_path_sim(word1,word2))
+		print("shortest_path_sim:", shortest_path_sim_indexes[i])
 		# Leacock CHodorow Similarity
 		leacock_chodorow_sim_indexes.append(leacock_chodorow(word1,word2))
 		print("leacock_chodorow nostro:",leacock_chodorow_sim_indexes[i])
@@ -108,27 +110,26 @@ def hypernym_paths(sense):
 			paths.append(ancestor_list)
 	return paths
 
-# shortest_path() returns the shortest path length from word1 to word2 considering the two senses that give the minimum path
+# shortest_path_sim(word1, word2) returns a similarity based on the shortest path length from word1 to word2 considering the two senses that give the minimum path
 # If no senses are found, the similarity is considered to be zero
-def shortest_path(word1, word2):
+def shortest_path_sim(word1, word2):
 	synset1 = wn.synsets(word1)
 	synset2 = wn.synsets(word2)
 	sim = None
+	sht_path = None
 	if(len(synset1)>0 and len(synset2)>0): # some words in the file WordSim353.tab are not present in WordNet DataBase
 		for s1 in synset1:
 			for s2 in synset2:
-				shortest_path_distance = shortest_path_aux(s1,s2)
-				if shortest_path_distance != -1:
-					new_sim = 2*depth_max-shortest_path_distance
-					if not sim or new_sim<sim: # not sim: first assignment, sim is None
-						sim = new_sim
+				tmp_shortest_path = shortest_path(s1, s2)
+				if (sht_path==None or tmp_shortest_path<sht_path) and tmp_shortest_path != -1:
+					sim = 2*depth_max-tmp_shortest_path
 	if sim:
 		return sim
 	else:
 		return 0
 
-# shortest_path_aux returns the minum paths between two senses
-def shortest_path_aux(sense1, sense2):
+# shortest_path(sense1, sense2) returns the minum paths between two senses
+def shortest_path(sense1, sense2):
 	paths1 = hypernym_paths(sense1)
 	paths2 = hypernym_paths(sense2)
 	best_len_path = None
@@ -139,9 +140,9 @@ def shortest_path_aux(sense1, sense2):
 					if path1[i]==path2[j]:
 						# if a match is found, the number of step from the senses to their common hypernym is computed
 						len_path = (len(path1)-1)-i + (len(path2)-1)-j
-						if not best_len_path or len_path<best_len_path: # not best_len_path: first assignment, best_first_path is None
+						if best_len_path==None or len_path<best_len_path: # not best_len_path: first assignment, best_first_path is None
 							best_len_path = len_path
-	if best_len_path:
+	if best_len_path!=None:
 		return best_len_path
 	else:
 		return -1
@@ -156,17 +157,18 @@ def leacock_chodorow(word1, word2):
 	if(len(synset1)>0 and len(synset2)>0): # some words in the file WordSim353.tab are not present in WordNet DataBase
 		for s1 in synset1:
 			for s2 in synset2:
-				length = shortest_path_aux(s1, s2)
-				if length != -1:
-					if(length == 0):
-						new_sim = abs(math.log((length+1)/(2.0*depth_max+1)))
-					else:
-						new_sim =  abs(math.log((length+1)/(2.0*depth_max)))
-					if(new_sim>sim and s1._pos==s2._pos):
-						sim = new_sim
-						sense1 = s1
-						sense2 = s2
-						found = True
+				if(s1.pos()==s2.pos()):
+					length = shortest_path(s1, s2)
+					if length != -1:
+						if(length == 0):
+							new_sim = abs(math.log((length+1)/(2.0*depth_max+1)))
+						else:
+							new_sim =  abs(math.log((length+1)/(2.0*depth_max)))
+						if new_sim>sim:
+							sim = new_sim
+							sense1 = s1
+							sense2 = s2
+							found = True
 	if(found):
 		print("leakock_chodorow WN: ", sense1.lch_similarity(sense2, simulate_root=False))
 		return sim
