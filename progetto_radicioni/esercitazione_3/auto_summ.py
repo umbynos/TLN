a4f67f7fb6db442e9abc8265d68ee04a08c5927a
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-07-09 17:05:03 +0200

    add nasari vectors and text files
    update auto_summ: it parses the sentences removing stopwords and punctuation

--- progetto_radicioni/esercitazione_3/auto_summ.py
+++ progetto_radicioni/esercitazione_3/auto_summ.py
@@ -1,45 +1,39 @@
 # Automatic Summarization with NASARI
 
 from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
 from itertools import islice
+import string
 
 
 # reads file starting from line 5 (title, then paragraphs)
 def main():
-    # per ogni file mi serve l'elenco delle parole senza stop words del titolo e di ogni paragrafo
-    trump_file = "text_data_modified/Donald-Trump-vs-Barack-Obama-on-Nuclear-Weapons-in-East-Asia.txt"
+    # files
+    trump_file = "data/Donald-Trump-vs-Barack-Obama-on-Nuclear-Weapons-in-East-Asia.txt"
+    smartphone_file = "data/People-Arent-Upgrading-Smartphones-as-Quickly-and-That-Is-Bad-for-Apple.txt"
+    moon_file = "data/The-Last-Man-on-the-Moon--Eugene-Cernan-gives-a-compelling-account.txt"
     # open file
     file = open(trump_file, "r", encoding="utf8")
-    # save title
+    # list of stopwords and punctuation
+    stop_words = stopwords.words('english') + list(string.punctuation)
+    # save title in title_set: it contains a set of non stop words included in the title
     for i in range(4):
         file.readline()
     title = file.readline()
-    title_words = []
-    for word in title.split():
-        title_words.append(word)
-    title_no_sw = remove_stop_words(title_words)
-    # salvo i paragrafi
-    for line in islice(file, 7, None):
-        print(line)
+    title_set = {word for word in title.split() if word not in stop_words}
+    # save sentences: list of sets. Each sets contains non stop words included in the sentence
+    sentences = []
+    for line in file:
+        if line != "\n":
+            for sentence in sent_tokenize(line):
+                sentences.append({word for word in sentence.split() if word not in stop_words})
     # poi sostituisco le parole con i vettori nasari
     # mi servono raggruppati per poter fare i confronti
 
 
 # creates NASARI vectors
-def file_to_vectors(file):
+def word_to_vectors(word):
     return 1
-
-
-# remove stop words
-def remove_stop_words(tokenized_words):
-    # Load stop words
-    stop_words = stopwords.words('english')
-    # Remove stop words
-    no_stopwords = []
-    for word in tokenized_words:
-        if word not in stop_words:
-            no_stopwords.append(word)
-    return no_stopwords
 
 
 if __name__ == "__main__":
