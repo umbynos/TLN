a9173d155c58db943979c77d55a5568578dd7e53
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-07-11 14:23:18 +0200

    create nasari_vectors and increased_nasari_vectors

--- progetto_radicioni/esercitazione_3/auto_summ.py
+++ progetto_radicioni/esercitazione_3/auto_summ.py
@@ -2,11 +2,19 @@
 
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
+from nltk.stem import WordNetLemmatizer
 import string
+import copy
 
 
 # reads file starting from line 5 (title, then paragraphs)
 def main():
+    # read NASARI file
+    nasari_file = open("dd-small-nasari-15.txt", "r", encoding="utf8")
+    nasari_dict = nasari_to_dict(nasari_file)
+    nasari_file.close()
+    # adding recursivly istances in nasari_dict
+    increased_nasari_dict = increase_nasari_dict(nasari_dict)
     # files
     trump_file = "data/Donald-Trump-vs-Barack-Obama-on-Nuclear-Weapons-in-East-Asia.txt"
     smartphone_file = "data/People-Arent-Upgrading-Smartphones-as-Quickly-and-That-Is-Bad-for-Apple.txt"
@@ -26,24 +34,37 @@ def main():
         if line != "\n":
             for sentence in sent_tokenize(line):
                 sentences_words.append({word for word in sentence.split() if word not in stop_words})
-    # poi sostituisco le parole con i vettori nasari
-    sentences_vectors = []
-    vectors_set = {}
-    for sentence in sentences_words:
-        for word in sentence:
-            vectors_set.add(word_to_vectors(word))
-        sentences_vectors.append(vectors_set)
-    # mi servono raggruppati per poter fare i confronti
+    # lemmatizzare le parole che vengono utilizzate come chiavi del dizionario
+    # weighted_overlap: cerchiamo le parole e i suoi sinonimi (-> key e valore) del titolo nelle frasi e calcoliamo overlap
+    # trovare un modo per sapere da quali frasi vengono le parole
+
+
 
 
 # creates NASARI vectors
 def word_to_vectors(word):
     # forse: se la parola è nei sinonimi di un vettore, allora ci associ tutti i sinonimi di quel vettore
+    # nei vettori mettiamo i lemmi -> le parole
     return 1
 
 
-def find_bn_id(word):
-    return 1
+def nasari_to_dict(nasari):
+    nasari_dict = {}
+    for line in nasari:
+        key = line.strip("\n").split(";")[1]
+        synset = line.split(";")[2:]
+        nasari_dict[key.lower()] = synset
+    return nasari_dict
+
+
+def increase_nasari_dict(nasari_dict):
+    nasari_dict_aux = copy.deepcopy(nasari_dict)
+    for key in nasari_dict:
+        for value in nasari_dict[key]:
+            if value in nasari_dict:
+                nasari_dict_aux[key].append(nasari_dict_aux[value])
+    return nasari_dict_aux
+
 
 
 if __name__ == "__main__":
