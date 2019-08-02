95cde0d6a4061b48f0ea5677aa63c3caf8ba757e
Umberto Baldi <34278123+umbynos@users.noreply.github.com>
2019-08-02 15:11:46 +0200

    Di caro project ro (#5)
    * add data: contains files
    add README: add part on tokenization
    add text_segmentation: it finds tokens from words in file
    
    * update README
    find initial pseudosentences
    
    * update README: to do
    
    * update README
    update text_segmentation: import similarity
    add __init__: request to use import
    add similarity: contains wu and palmer similarity
    
    * update README
    update text_segmentation: compute weights for words in each pseudosentence. Test with first pseudo sentence: all zero. Check others
    
    * update README: read for what to do
    update text_segmentation: it divides pseudo sentences in blocks of length k
    
    * update text_segmentation: fix bug in list_pseudosentences (it didn't record last pseudo sentence)
    
    * update text_segmentation: fix bug in find_blocks (it didn't record last block)
    
    * update text_segmentation: fix tokenize_words (now use words, not letters); modify find_weights in find_similarities (it computes wup divided in blocks)
    update similarity: comment print line
    
    * update text_segmentation: modify find_similarities (now it returns a dictionari: key (pseudo sentece 1, pseud sentence 2); value wup between what is in key
    
    * update text_segmentation: implements find_new_blocks
    to_do: execute algorithm n times in order to obtain better results
    
    * update text_segmentation: fix bug (last pseudo sentences weren't put in new_block
    files: remove blank lines
    
    * update text_segmentation: add iteration for the algorithm + modify output
    
    * update text_segmentation: fix bug (now it doesn't create empty blocks)
    
    * update README.md
    
    * Delete __init__.py

---
+++ progetto_di_caro_ro/README.md
@@ -0,0 +1,79 @@
+# Text Segmentation
+Implementare un semplice sistema di segmentazione
+di testi ispirato al Text Tiling, e che usi / integri risorse
+come WordNet, named entities, etc.
+
+Utilizzo WordNet: https://www.nltk.org/_modules/nltk/tokenize/texttiling.html
+
+## Text Tiling Algorithm
+Credits: https://www.aclweb.org/anthology/J97-1003
+TextTiling: Segmenting Text into
+Multi-paragraph Subtopic Passages
+Marti A. Hearst*, Xerox PARC  
+Da pagina 16  
+The Text Tiling algorithm for discovering subtopic structure
+using term repetition has three main parts:
+1. Tokenization
+2. Lexical Score Determination
+3. Boundary Identification
+
+### Tokenization
+- Divide input text into individual lexical units (list of words)
+- Skip title
+- Convert to lower-case
+- Skip stop words and punctuation
+- Reduce tokens to their morphemes
+- Subdivide text into pseudosentences of a predefined
+ size w (equal-sized units: number of shared terms
+between two long sentences and between a long and a short
+sentence would probably yield incomparable scores; sentences
+are too short to expect normalization to really accommodate
+for the differences) [token-sequences]
+- Morphologically analyzed token is stored in a table along
+with a record of the token-sequence number
+it occurred in, and the number of times it appeared in the 
+token-sequence
+- A record is also kept of the locations of the paragraph
+breaks within the text  
+NOTA: questo ed il record precedente non sono stati fatti
+- Stop words contribute to the computation of the size of
+the token-sequence, but not to the computation of
+the similarity between blocks of text.
+
+### Lexical Score Determination
+Vediamo due tipi di score:
+- Block comparison: confronta blocchi adiacenti di testi per
+vedere quanto sono simili in base a quante parole hanno in
+comune (similarità lessicale). k: dimensione dei blocchi.  
+La similarità si calcola per ogni sequenza di token: viene assegnato
+uno score ad ogni gap di sequenze di token i in base a quanto
+i blocchi che vanno da i-k a i sono simili ai blocchi che vanno
+da i+1 a i+k+1.  
+Similarità tra blocchi: siano b1={token-sequence_{i-k},...,
+token-sequence_{i}} e b2={token-sequence_{i+l},...,
+token-sequence_{i+k+l}}, allora score = formula pag 17
+- Vocabulary Introduxtion Method: assegna uno score ad una
+sequeza di token in base a quante nuove parole ci sono 
+nell'intervallo in cui si trova il punto di mezzo (midpoint)  
+Numero di parole nuove / numero di parole totali
+
+NOTA: si utilizzano le pseudo sentences perché i paragrafi
+hanno una lunghezza irregolare che porterebbe ad un confronto
+sbilanciato. Per usarli ci andrebbe una normalizzazione  
+
+Non utilizzo nessuno dei due: utilizzo WordNet e quindi come
+valutazione di similarità utilizzo Wu & Palmer o Leacock Chodorow
+
+### Boundary Identification
+
+
+
+# COSA FA
+w: dimensione pseudo frasi  
+k: numero di pseudo frasi all'interno dei blocchi
+
+- Dividi la frase in blocchi di dimensione fissa
+- dentro ad ogni blocco confronti le parole in pseudo frasi
+contigue
+- spezzi il blocco dovre trovi il minimo trovato prima
+(ignori quindi la divisione che avevi in precedenza)
---
+++ progetto_di_caro_ro/text_segmentation.py
@@ -0,0 +1,182 @@
+# Text Segmentation with Text Tiling Method
+
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import progetto_di_caro_ro.similarity as sim
+import string
+import copy
+
+
+def main():
+    # Tokenization
+    # files
+    trump_file = "data/Donald-Trump-vs-Barack-Obama-on-Nuclear-Weapons-in-East-Asia.txt"
+    smartphone_file = "data/People-Arent-Upgrading-Smartphones-as-Quickly-and-That-Is-Bad-for-Apple.txt"
+    moon_file = "data/The-Last-Man-on-the-Moon--Eugene-Cernan-gives-a-compelling-account.txt"
+    # divide input text into individual lexical units (list of words)
+    words = extrapolate_file(moon_file)
+    # Subdivide text into pseudosentences of a predefined size w
+    w = 10
+    pseudosentences = list_pseudosentences(w, words)
+    # find k: number of sentences in each block
+    # each block contains 10% of the pseudosentences text
+    k = int(0.1 * len(pseudosentences))  # SOLITAMENTE QUANTI ARGOMENTI AFFRONTA UN TESTO?
+    # put pseudosentences in blocks of length k
+    # block type: [strings]
+    blocks = find_blocks(pseudosentences, k)
+    # Lexical Score Determination
+    dict_similarities = find_similarities(blocks)
+    # Boundary Identification
+    iteration_number = 3
+    new_segmentation = find_new_boundaries(iteration_number, dict_similarities, blocks)
+    print("###########################")
+    print("## OLD SEGMENTS' LENGTH ##")
+    print("###########################")
+    for i, block in enumerate(blocks):
+        print(str(i) + " " + str(len(block)))
+    print("###########################")
+    print("## NEW SEGMENTS' LENGTH ##")
+    print("###########################")
+    for i, block in enumerate(new_segmentation):
+        print(str(i) + " " + str(len(block)))
+    print("###########################")
+    print("###### OLD SEGMENTS ######")
+    print("###########################")
+    for block in blocks:
+        for pseudo_sentence in block:
+            print(pseudo_sentence)
+        print()
+    print("###########################")
+    print("###### NEW SEGMENTS ######")
+    print("###########################")
+    for block in new_segmentation:
+        for pseudo_sentence in block:
+            print(pseudo_sentence)
+        print()
+
+
+# divide input text into individual lexical units (list of words)
+def extrapolate_file(file_to_open):
+    file = open(file_to_open, "r", encoding="utf8")
+    words = []
+    for line in file:
+        for word in word_tokenize(line):
+            words.append(word.lower())
+    file.close()
+    return words
+
+
+# remove stop words and punctuation + reduce to lemma
+# sw_punct: list containing english stop words and punctuation
+def tokenize_words(words):
+    tokens = []
+    sw_punct = stopwords.words('english') + list(string.punctuation)
+    lemmatizer = WordNetLemmatizer()
+    for word in word_tokenize(words):
+        if word not in sw_punct:
+            tokens.append(lemmatizer.lemmatize(word))
+    return tokens
+
+
+# Subdivide text into pseudosentences of a predefined size w
+def list_pseudosentences(w, words):
+    pseudosentences = []
+    pseudosentence = ""
+    count = w
+    for i in range(len(words)):
+        if count > 0:
+            if words[i] not in list(string.punctuation):
+                pseudosentence += words[i] + " "
+                count -= 1
+        else:
+            count = w
+            pseudosentences.append(pseudosentence)
+            pseudosentence = "" + words[i] + " "
+    pseudosentences.append(pseudosentence)
+    return pseudosentences
+
+
+# put pseudosentences in blocks of length k
+def find_blocks(pseudosentences, k):
+    count = k
+    blocks = []
+    block = []
+    for pseudosent in pseudosentences:
+        if count > 0:
+            block.append(pseudosent)
+            count -= 1
+        else:
+            count = k
+            blocks.append(block)
+            block = []
+    if block:  # if block isn't empty
+        blocks.append(block)
+    return blocks
+
+
+# compute wup similarity for every word in the first pseudo sentence with every word of the second
+def find_similarities(blocks):
+    dict_similarities = {}
+    pseudo_sent_pos = 0
+    for block in blocks:
+        for i in range(len(block)):
+            if i+1 < len(block):
+                # token:  no stopwords and punctuation + lemma
+                tokens1 = tokenize_words(block[i])
+                tokens2 = tokenize_words(block[i+1])
+                wups = []
+                for token1 in tokens1:
+                    for token2 in tokens2:
+                        wups.append(sim.wu_and_palmer(token1, token2))
+                # average
+                weight = 0.0
+                for wup in wups:
+                    weight += wup
+                if len(wups) > 0:
+                    dict_similarities[(pseudo_sent_pos, pseudo_sent_pos+1)] = weight / len(wups)  # O SOLO IL PESO SENZA DIVIDERE???
+                pseudo_sent_pos += 1
+    return dict_similarities
+
+
+def find_new_boundaries(iteration_number, dict_similarities, blocks):
+    aux_blocks = copy.deepcopy(blocks)
+    while iteration_number > 0:
+        aux_blocks = copy.deepcopy(find_new_blocks(dict_similarities, aux_blocks))
+        iteration_number -= 1
+    return aux_blocks
+
+
+def find_new_blocks(dict_similarities, blocks):
+    new_blocks = []
+    new_block = []
+    pseudo_sent_pos = 0
+    bad_pseudosent_pos = 1  # if the minimum is between the first couple, is moved the pseudosentence with index 1
+    for b, block in enumerate(blocks):
+        if len(block) > 1:
+            minimum = dict_similarities[(pseudo_sent_pos, pseudo_sent_pos+1)]
+            bad_pseudosent_pos = 1
+            for ps in range(len(block)-1):  # -1: if last one is considered it goes out of range
+                actual_sim = dict_similarities[(pseudo_sent_pos, pseudo_sent_pos+1)]
+                if actual_sim < minimum:
+                    minimum = actual_sim
+                    bad_pseudosent_pos = ps+1
+                pseudo_sent_pos += 1
+            for k in range(bad_pseudosent_pos):
+                new_block.append(block[k])
+        else:
+            new_block.append(block[0])
+        new_blocks.append(new_block)
+        new_block = []
+        remaining = bad_pseudosent_pos
+        while remaining < len(block):
+            new_block.append(block[remaining])
+            remaining += 1
+    if new_block:  # if new_block isn't empty
+        new_blocks.append(new_block)
+    return new_blocks
+
+
+if __name__ == "__main__":
+    main()
